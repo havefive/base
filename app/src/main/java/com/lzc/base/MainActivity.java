b07f8ecba198b961ee8622f8f01e8ab9cb2adba1
havefive <lzc09008@gmail.com>
2015-10-02 21:10:22 +0800

    加入容云，待测待完善

--- app/src/main/java/com/lzc/base/MainActivity.java
+++ app/src/main/java/com/lzc/base/MainActivity.java
@@ -2,12 +2,16 @@ package com.lzc.base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Toast;
 
 import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.message.PushAgent;
+
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,31 @@ public class MainActivity extends AppCompatActivity {
         PushAgent mPushAgent = PushAgent.getInstance(this.getBaseContext());
         mPushAgent.enable();
         PushAgent.getInstance(this.getBaseContext()).onAppStart();
+
+        //容云
+//        String Token = "d6bCQsXiupB/4OyGkh+TOrI6ZiT8q7s0UEaMPWY0lMxmHdi1v/AAJxOma4aYXyaivfPIJjNHdE+FMH9kV/Jrxg==";//test
+//        /**
+//         * IMKit SDK调用第二步
+//         *
+//         * 建立与服务器的连接
+//         *
+//         */
+//        RongIM.connect(Token, new RongIMClient.ConnectCallback() {
+//            @Override
+//            public void onTokenIncorrect() {
+//                //Connect Token 失效的状态处理，需要重新获取 Token
+//            }
+//
+//            @Override
+//            public void onSuccess(String userId) {
+//                Log.e("MainActivity", "——onSuccess— -"+userId);
+//            }
+//
+//            @Override
+//            public void onError(RongIMClient.ErrorCode errorCode) {
+//                Log.e("MainActivity", "——onError— -"+errorCode);
+//            }
+//        });
 
     }
 
