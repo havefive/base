b404e2680f5157c4e12c0fbe2cfe42c9a639d4d8
havefive <lzc09008@gmail.com>
2015-12-05 20:25:52 +0800

    集成swipelayout

---
+++ app/src/main/java/com/lzc/base/ui/SwipeActivity.java
@@ -0,0 +1,85 @@
+package com.lzc.base.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.daimajia.swipe.SwipeLayout;
+import com.lzc.base.R;
+import com.nineoldandroids.view.ViewHelper;
+
+public class SwipeActivity extends AppCompatActivity {
+
+    private SwipeLayout sample1, sample2, sample3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe);
+
+
+        sample1 = (SwipeLayout) findViewById(R.id.sample1);
+        sample1.setShowMode(SwipeLayout.ShowMode.PullOut);
+        View starBottView = sample1.findViewById(R.id.starbott);
+        sample1.addDrag(SwipeLayout.DragEdge.Left, sample1.findViewById(R.id.bottom_wrapper));
+        sample1.addDrag(SwipeLayout.DragEdge.Right, sample1.findViewById(R.id.bottom_wrapper_2));
+        sample1.addDrag(SwipeLayout.DragEdge.Top, starBottView);
+        sample1.addDrag(SwipeLayout.DragEdge.Bottom, starBottView);
+        sample1.addRevealListener(R.id.delete, new SwipeLayout.OnRevealListener() {
+            @Override
+            public void onReveal(View child, SwipeLayout.DragEdge edge, float fraction, int distance) {
+
+            }
+        });
+
+        sample1.getSurfaceView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SwipeActivity.this, "Click on surface", Toast.LENGTH_SHORT).show();
+                Log.d(SwipeActivity.class.getName(), "click on surface");
+            }
+        });
+        sample1.getSurfaceView().setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(SwipeActivity.this, "longClick on surface", Toast.LENGTH_SHORT).show();
+                Log.d(SwipeActivity.class.getName(), "longClick on surface");
+                return true;
+            }
+        });
+        sample1.findViewById(R.id.star2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SwipeActivity.this, "Star", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sample1.findViewById(R.id.trash2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SwipeActivity.this, "Trash Bin", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sample1.findViewById(R.id.magnifier2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SwipeActivity.this, "Magnifier", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sample1.addRevealListener(R.id.starbott, new SwipeLayout.OnRevealListener() {
+            @Override
+            public void onReveal(View child, SwipeLayout.DragEdge edge, float fraction, int distance) {
+                View star = child.findViewById(R.id.star);
+                float d = child.getHeight() / 2 - star.getHeight() / 2;
+                ViewHelper.setTranslationY(star, d * fraction);
+                ViewHelper.setScaleX(star, fraction + 0.6f);
+                ViewHelper.setScaleY(star, fraction + 0.6f);
+            }
+        });
+
+    }
+}
